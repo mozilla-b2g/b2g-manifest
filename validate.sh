01c0e533ac59c01c29b16ab572f62de531fd4710
Pete Moore <pmoore@mozilla.com>
2014-11-06 15:48:48 +0100

    Revert "fixes"
    This reverts commit f5601f24a5c47be9dac8cdebad33f724ada040ea.

--- validate.sh
+++ validate.sh
@@ -1,30 +1,8 @@
-#!/bin/bash -euxv
+#!/bin/bash
 
 echo "Installing mozharness..."
-B2GBUMPER_DIR="$(pwd)"
-cd ..
-echo "Current directory: '$(pwd)'"
-git clone -b bug1091084 https://github.com/petemoore/build-mozharness mozharness
-echo "Installing tools..."
-git clone https://github.com/mozilla/build-tools tools
-
-HGTOOL_PATH="$(find "$(pwd)/tools" -name hgtool.py)"
-GITTOOL_PATH="$(find "$(pwd)/tools" -name gittool.py)"
-
-function replace {
-    local file="${1}"
-    local tokenname="${2}"
-    local replacestring="${3}"
-    # don't use sed -i so we can test on mac
-    mv "${file}" "${file}.orig"
-    cat "${file}.orig" | sed "s*${tokenname}*${replacestring}*g" > "${file}"
-    rm "${file}.orig"
-}
-
-# update references to hgtool.py and gittool.py to correct locations...
-replace "${B2GBUMPER_DIR}/travis-mozharness-config.py" HGTOOL "${HGTOOL_PATH}"
-replace "${B2GBUMPER_DIR}/travis-mozharness-config.py" GITTOOL "${HGTOOL_PATH}"
-
-mozharness/scripts/b2g_bumper.py -c "${B2GBUMPER_DIR}/travis-mozharness-config.py" --no-check-treestatus
+pwd
+git clone https://github.com/mozilla/build-mozharness mozharness
+mozharness/scripts/b2g_bumper.py -c travis-mozharness-config.py --no-check-treestatus
 
 echo "Running CI tests..."
