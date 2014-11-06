8399ace3d2375fc1fb5c581422b06fc7a84bf711
Pete Moore <pmoore@mozilla.com>
2014-11-06 15:48:50 +0100

    Revert "stuff"
    This reverts commit 3e7efab772906c7db9b1779a524d17c656626613.

--- validate.sh
+++ validate.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-echo "Installing mozharness..."
-pwd
-git clone https://github.com/mozilla/build-mozharness mozharness
-mozharness/scripts/b2g_bumper.py -c travis-mozharness-config.py --no-check-treestatus
-
 echo "Running CI tests..."
+:
